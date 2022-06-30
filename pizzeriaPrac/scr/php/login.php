a4dc0da23fd6ce1c4a506a7491c35ba6a8a83a6b
Eduard Chervonenko <59712111+Ra1n-xD@users.noreply.github.com>
2022-07-01 01:21:09 +0300

    Add files via upload

--- pizzeriaPrac/scr/php/login.php
+++ pizzeriaPrac/scr/php/login.php
@@ -1,37 +1,38 @@
 <?php
 include '../include/header.php';
 ?>
-<div class="container col-8">
-    <div class="row pt-4">
-        <div class="container bg-white col-6">
+<div class="container col-10">
+    <div class="row pt-5">
+        <div class="container bg-white col-5">
             <h3>Авторизация</h3>
-            <form action="authorization.php" method="POST">
-                <p>Имя:
-                    <input type="text" name="F_Name" required />
-                </p>
-                <p>Фамилия:
-                    <input type="text" name="S_Name" required />
-                </p>
-                <input class="btn btn-outline-secondary" type="submit" value="Подтвердить ввод">
+            <form action="../include/signup.php" method="POST">
+                <label>Почта</label>
+                <input type="email" class="form-control" aria-describedby="emailHelp">
+
+                <label class="mt-3">Пароль</label>
+                <input type="password" class="form-control">
+
+                <button type="submit" class="mt-3 btn btn-primary">Войти</button>
             </form>
         </div>
 
-        <div class="container bg-white col-md-6">
+        <div class="container bg-white col-5">
             <h3>Регистрация</h3>
-            <form action="register.php" method="POST">
-                <p>Имя:
-                    <input type="text" name="F_Name" required />
-                </p>
-                <p>Фамилия:
-                    <input type="text" name="S_Name" required />
-                </p>
-                <p>Номер телефона:
-                    <input type="number" name="T_Number" required />
-                </p>
-                <p>Почта:
-                    <input type="email" name="Mail" required />
-                </p>
-                <input class="btn btn-outline-secondary" type="submit" value="Подтвердить ввод">
+            <form action="" method="POST">
+
+                <label>Имя</label>
+                <input type="text" class="form-control">
+
+                <label class="mt-3">Почта</label>
+                <input type="email" class="form-control" aria-describedby="emailHelp">
+
+                <label class="mt-3">Пароль</label>
+                <input type="password" class="form-control">
+
+                <label class="mt-3">Подтверждение пароля</label>
+                <input type="password" class="form-control">
+
+                <button type="submit" class="mt-3 btn btn-primary">Зарегистрироватся</button>
             </form>
         </div>
     </div>
